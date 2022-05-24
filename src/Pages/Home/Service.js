1ab7daa2dcbd252548f101da70ed08bcc5f9a47a
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-24 19:18:58 +0600

    add tools section

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -3,21 +3,21 @@ import React from 'react';
 const Service = ({ service }) => {
   const { img, name, description, quantity, available, price } = service;
   return (
-    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
         <img
           src={img}
           alt="Tools"
         />
       </figure>
-      <div class="card-body items-center text-center">
-        <h2 class="font-bold text-3xl">{name}</h2>
+      <div className="card-body items-center text-center">
+        <h2 className="font-bold text-3xl">{name}</h2>
         <h3 className='text-xl'>Minimum order quantity: {quantity}</h3>
         <h3 className='text-xl'>Available quantity: {available}</h3>
         <h3 className='text-xl'>Per unit Price: ${price}</h3>
         <h3>{description}</h3>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Purchase</button>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Purchase</button>
         </div>
       </div>
     </div>
--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -2,26 +2,23 @@ import React, { useEffect, useState } from 'react';
 import Service from './Service';
 
 const Tools = () => {
-    const [services, setServices] = useState([]);
+  const [services, setServices] = useState([]);
 
-    useEffect( () =>{
-        fetch('services.json')
-        .then(res => res.json())
-        .then(data => setServices(data));
-    }, [])
-    return (
-        <div className='mt-10 text-center'>
-            <h2 className='font-bold text-4xl'>New Tools On Market</h2>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-                {
-                    services.map(service=><Service
-                    key={service._id}
-                    service={service}
-                    ></Service>)
-                }
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch('http://localhost:5000/service')
+      .then(res => res.json())
+      .then(data => setServices(data));
+  }, []);
+  return (
+    <div className="mt-10 text-center">
+      <h2 className="font-bold text-4xl">New Tools On Market</h2>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {services.map(service => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tools;
