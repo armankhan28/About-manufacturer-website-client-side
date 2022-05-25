5dee27d2ea24cc10d2b9a1d9ccab2816d9d958c6
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-26 05:55:03 +0600

    add useAdmin

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -5,64 +5,64 @@ import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
-    const [orders, setOders] = useState([]);
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate();
+  const [orders, setOders] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
-
-    useEffect(() =>{
-       if(user){
-        fetch(`http://localhost:5000/booking?client=${user.email}`,{
+  useEffect(() => {
+    if (user) {
+      fetch(
+        `https://desolate-fortress-23405.herokuapp.com/booking?client=${user.email}`,
+        {
           method: 'GET',
           headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`,
           },
-        })
+        }
+      )
         .then(res => {
-          console.log('res',res);
+          console.log('res', res);
           if (res.status === 401 || res.status === 403) {
             signOut(auth);
             localStorage.removeItem('accessToken');
             navigate('/');
           }
-          return res.json()
+          return res.json();
         })
         .then(data => {
           setOders(data);
         });
-       }
-    } ,[user])
+    }
+  }, [user]);
 
-    return (
-        <div>
-            <h2>My Orders{orders.length}</h2>
-            <div class="overflow-x-auto">
-  <table class="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Product</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Pay</th>
-      </tr>
-    </thead>
-    <tbody>
-        {
-            orders.map((o, index) => 
-                <tr key={o._id}>
+  return (
+    <div>
+      <h2>My Orders{orders.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Pay</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((o, index) => (
+              <tr key={o._id}>
                 <th>{index + 1}</th>
                 <td>Product</td>
                 <td>{o.Name}</td>
                 <td>{o.client}</td>
-              </tr> )
-        }
-      
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
